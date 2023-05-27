a7543d4f51217fc8a92d6cb9f6e3a8271c445d1a
Rolando Martinez <93880287+rolandomart@users.noreply.github.com>
2023-05-26 22:53:51 -0600

    V3.0

--- script.js
+++ script.js
@@ -1,89 +1,99 @@
+// Animación de escritura en el placeholder
+const textarea = document.getElementById('texto');
+const placeholderText = textarea.getAttribute('placeholder');
+
+for (let i = 0; i <= placeholderText.length; i++) {
+  setTimeout(() => {
+    textarea.setAttribute('placeholder', placeholderText.substring(0, i));
+  }, i * 90);
+}
+
+// Cambiar tema de fondo
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("change", function () {
+  if (this.checked) {
+    enableDarkMode();
+  } else {
+    disableDarkMode();
+  }
+});
+
+function enableDarkMode() {
+  document.body.classList.add("dark-mode");
+}
+
+function disableDarkMode() {
+  document.body.classList.remove("dark-mode");
+}
+
+
+// Función para convertir el texto a minúsculas en tiempo real
+function convertirAMinusculas() {
+  const texto = document.getElementById('texto');
+  texto.value = texto.value.toLowerCase();
+}
+
+// Agregar el evento para convertir a minúsculas en tiempo real
+document.getElementById('texto').addEventListener('input', convertirAMinusculas);
+
+
+
 // Función para encriptar texto
 function encriptar() {
-  let texto = document.getElementById('texto').value;
+  const texto = document.getElementById('texto').value;
 
   if (texto === "") {
-    document.getElementById('encriptar').disabled = true;
-    document.getElementById('desencriptar').disabled = true;
-    document.getElementById('limpiar').disabled = true;
+    deshabilitarBotones();
     return;
   }
 
-  let mensaje = "";
+  const mensaje = texto.toLowerCase()
+    .replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/a/g, "ai")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat");
 
-  for (let i = 0; i < texto.length; i++) {
-    switch (texto[i]) {
-      case "e":
-        mensaje += "enter";
-        break;
-      case "i":
-        mensaje += "imes";
-        break;
-      case "a":
-        mensaje += "ai";
-        break;
-      case "o":
-        mensaje += "ober";
-        break;
-      case "u":
-        mensaje += "ufat";
-        break;
-      default:
-        mensaje += texto[i];
-    }
-  }
-
-  document.getElementById('mensaje').value = mensaje;
-  document.getElementById('titulo-mensaje').innerHTML = "Texto encriptado";
-  document.getElementById('msj').innerHTML = "Texto encriptado correctamente.";
-  document.getElementById('image').classList.add('invisible');
-  document.getElementById('result').classList.remove('invisible');
-  document.getElementById('copiar').disabled = false;
-  document.getElementById('texto').addEventListener('input', validarTexto);
-
+  actualizarMensaje(mensaje, "Texto encriptado", "Texto encriptado correctamente.");
 }
 
 // Función para desencriptar texto
 function desencriptar() {
-  let mensaje = document.getElementById('mensaje').value;
+  const mensaje = document.getElementById('mensaje').value;
 
   if (mensaje === "") {
-    document.getElementById('encriptar').disabled = true;
-    document.getElementById('desencriptar').disabled = true;
-    document.getElementById('limpiar').disabled = true;
+    deshabilitarBotones();
     return;
   }
 
-  let texto = "";
+  const texto = mensaje.replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
 
-  for (let i = 0; i < mensaje.length; i++) {
-    if (mensaje.substring(i, i + 5) === "enter") {
-      texto += "e";
-      i += 4;
-    } else if (mensaje.substring(i, i + 4) === "imes") {
-      texto += "i";
-      i += 3;
-    } else if (mensaje.substring(i, i + 2) === "ai") {
-      texto += "a";
-      i += 1;
-    } else if (mensaje.substring(i, i + 4) === "ober") {
-      texto += "o";
-      i += 3;
-    } else if (mensaje.substring(i, i + 4) === "ufat") {
-      texto += "u";
-      i += 3;
-    } else {
-      texto += mensaje[i];
-    }
-  }
+  actualizarMensaje(texto, "Texto desencriptado", "Texto desencriptado correctamente.");
+}
 
+
+
+// Función para actualizar el mensaje y mostrar resultados
+function actualizarMensaje(texto, titulo, msj) {
   document.getElementById('mensaje').value = texto;
-  document.getElementById('titulo-mensaje').innerHTML = "Texto desencriptado";
-  document.getElementById('msj').innerHTML = "Texto desencriptado correctamente.";
+  document.getElementById('titulo-mensaje').innerHTML = titulo;
+  document.getElementById('msj').innerHTML = msj;
   document.getElementById('image').classList.add('invisible');
   document.getElementById('result').classList.remove('invisible');
-  document.getElementById('copiar').disabled = false;
+  document.getElementById('copiarBtn').disabled = false;
   document.getElementById('texto').addEventListener('input', validarTexto);
+}
+
+// Función para deshabilitar botones
+function deshabilitarBotones() {
+  document.getElementById('encriptar').disabled = true;
+  document.getElementById('desencriptar').disabled = true;
+  document.getElementById('limpiar').disabled = true;
 }
 
 // Función para limpiar texto
@@ -94,27 +104,46 @@ function limpiar() {
   document.getElementById('msj').innerHTML = "Ingresa el texto que desees encriptar o desencriptar.";
   document.getElementById('image').classList.remove('invisible');
   document.getElementById('result').classList.add('invisible');
-  document.getElementById('copiar').disabled = true;
+  document.getElementById('copiarBtn').disabled = true;
 
   // Restablecer todos los valores a sus estados iniciales
-  document.getElementById('copiar').innerHTML = "Copiar";
+  document.getElementById('copiarBtn').innerHTML = "Copiar";
   document.getElementById('texto').disabled = false;
   document.getElementById('mensaje').disabled = false;
   document.getElementById('encriptar').disabled = false;
   document.getElementById('desencriptar').disabled = false;
 }
 
-
-// Función para copiar texto al portapapeles
+// Función para copiar el mensaje encriptado
 function copiar() {
-  let mensaje = document.getElementById('mensaje');
+  const mensaje = document.getElementById('mensaje');
   mensaje.select();
   mensaje.setSelectionRange(0, 99999);
   document.execCommand("copy");
-  document.getElementById('copiar').innerHTML = "¡Copiado!";
-  document.getElementById('copiar').style.backgroundColor = "#364248"; // Cambiar el color del botón
+
+  // Actualizar el contenido y estilo del botón de copiar mensaje encriptado
+  const copiarBtn = document.getElementById('copiarBtn');
+  copiarBtn.innerHTML = "¡Copiado!";
+  copiarBtn.style.backgroundColor = "#364248";
   setTimeout(function () {
-    document.getElementById('copiar').innerHTML = "Copiar";
-    document.getElementById('copiar').style.backgroundColor = ""; // Volver al color original del botón
-  }, 2000); // 2 segundos de espera
+    copiarBtn.innerHTML = "Copiar";
+    copiarBtn.style.backgroundColor = "";
+  }, 2000);
+}
+
+// Función para copiar el texto encriptado
+function copiarTexto() {
+  const mensaje = document.getElementById('mensaje');
+  mensaje.select();
+  mensaje.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+
+  // Actualizar el contenido y estilo del botón de copiar texto encriptado
+  const copiarTextoBtn = document.getElementById('copiarTextoBtn');
+  copiarTextoBtn.innerHTML = "¡Copiado!";
+  copiarTextoBtn.style.backgroundColor = "#364248";
+  setTimeout(function () {
+    copiarTextoBtn.innerHTML = "Copiar";
+    copiarTextoBtn.style.backgroundColor = "";
+  }, 2000);
 }
